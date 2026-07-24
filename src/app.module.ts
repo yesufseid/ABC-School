afddcb122623fe1f536953643d4e04f021e30d5b
sweatshirt <sweatshirtblue2@gmail.com>
2026-07-24 22:56:03 +0300

    chore: subscription based authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { DatabaseService } from './modules/database/database.service';
 import { APP_GUARD } from '@nestjs/core';
 import { AppGuard } from './modules/auth/guards/app.guard';
 import { RolesGuard } from './modules/auth/guards/roles.guard';
+import { SubscriptionGuard } from './modules/auth/guards/subscription.guard';
 
 // TODO: refactor config module and transactional db setup
 @Module({
@@ -45,6 +46,10 @@ import { RolesGuard } from './modules/auth/guards/roles.guard';
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: SubscriptionGuard,
     },
   ],
 })
